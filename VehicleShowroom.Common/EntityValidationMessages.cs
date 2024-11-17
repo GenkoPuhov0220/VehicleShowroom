76649e2e24aaa3c5a16be9e8cda8ac50110175db
GenkoPuhov GenkoPuhov <GenkoPuhov@students.softuni.bg>
2024-11-17 12:56:03 +0200

    Add entity validation messages

---
+++ VehicleShowroom.Common/EntityValidationMessages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleShowroom.Common
+{
+    public static class EntityValidationMessages
+    {
+        //Vehicle
+        public const string VehicleTypeMessages = "Vehicle type is required";
+        public const string VehicleMakeMessages = "Make is required.";
+        public const string VehicleMakeMinLenght = "Make cannot exceed 2 characters.";
+        public const string VehicleMakeMaxLenght = "Make cannot exceed 150 characters.";
+
+
+    }
+}
