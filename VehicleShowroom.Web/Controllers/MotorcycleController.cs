cb523c0aff9a29ca433a5503029ddd8a100367eb
GenkoPuhov GenkoPuhov <GenkoPuhov@students.softuni.bg>
2024-11-22 13:55:01 +0200

    Add motorcycle edit

--- VehicleShowroom.Web/Controllers/MotorcycleController.cs
+++ VehicleShowroom.Web/Controllers/MotorcycleController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 using VehicleShowroom.Data;
 
 namespace VehicleShowroom.Web.Controllers
 {
+    using static VehicleShowroom.Common.EntityValidationConstants;
     public class MotorcycleController : Controller
     {
         private readonly VehicleDbContext context;
@@ -47,6 +49,78 @@ namespace VehicleShowroom.Web.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(motorcycle);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var vehicle = await context
+               .Motorcycles
+               .Include(c => c.Vehicle)
+               .Where(c => c.VehicleId == id)
+               .Select(c => new MotorcycleEditViewModel
+               {
+
+                   VehicleType = c.Vehicle.VehicleType,
+                   Make = c.Vehicle.Make,
+                   Model = c.Vehicle.Model,
+                   Year = c.Vehicle.Year.ToString(YearFormating),
+                   Price = c.Vehicle.Price,
+                   Color = c.Vehicle.Color,
+                   FuelType = c.Vehicle.FuelType,
+                   ImageUrl = c.Vehicle.ImageUrl,
+
+                   MotorcycleId = c.MotorcycleId,
+                   Kw = c.Kw,
+
+               })
+                .FirstOrDefaultAsync();
+
+            if (vehicle == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(vehicle);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(MotorcycleEditViewModel models)
+        {
+            bool IsYearValid = DateTime
+               .TryParseExact(models.Year, YearFormating, CultureInfo.InvariantCulture, DateTimeStyles.None,
+               out DateTime yearValid);
+
+            if (!IsYearValid)
+            {
+                ModelState.AddModelError(nameof(models.Year), "The Year must be in the following format: dd/MM/yyyy");
+
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(models);
+            }
+
+            var motorcycle = await context
+                .Motorcycles
+                .Include(c => c.Vehicle)
+                .FirstOrDefaultAsync(c => c.MotorcycleId == models.MotorcycleId);
+            if (motorcycle == null)
+            {
+                return NotFound();
+            }
+
+            motorcycle.Vehicle.VehicleType = models.VehicleType;
+            motorcycle.Vehicle.Make = models.Make;
+            motorcycle.Vehicle.Model = models.Model;
+            motorcycle.Vehicle.Year = yearValid;
+            motorcycle.Vehicle.Price = models.Price;
+            motorcycle.Vehicle.Color = models.Color;
+            motorcycle.Vehicle.FuelType = models.FuelType;
+            motorcycle.Vehicle.ImageUrl = models.ImageUrl;
+
+            motorcycle.Kw = models.Kw;
+            await context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ VehicleShowroom.Web/Model/Motorcycle/MotorcycleEditViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace VehicleShowroom.Web
+{
+    using System.ComponentModel.DataAnnotations;
+    using static VehicleShowroom.Common.EntityValidationMessages;
+
+    public class MotorcycleEditViewModel
+    {
+        public int VehicleId { get; set; }
+
+        [Required(ErrorMessage = VehicleTypeMessages)]
+        public string VehicleType { get; set; } = null!;
+
+        [Required(ErrorMessage = VehicleMakeMessages)]
+        public string Make { get; set; } = null!;
+
+        [Required(ErrorMessage = VehicleModelMessages)]
+        public string Model { get; set; } = null!;
+
+        [Required]
+        public string Year { get; set; } = null!;
+
+        [Required(ErrorMessage = VehiclePriceMessages)]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = VehicleColorMessages)]
+        public string Color { get; set; } = null!;
+
+        [Required(ErrorMessage = VehicleFuelTypeMessages)]
+        public string FuelType { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        public int MotorcycleId { get; set; }
+        public int Kw { get; set; }
+    }
+}
