8137753e00b3d1eaabaed4e12a6db5f26b2fc820
GenkoPuhov GenkoPuhov <GenkoPuhov@students.softuni.bg>
2024-11-22 13:47:52 +0200

    Add SuperCar edit

--- VehicleShowroom.Web/Controllers/SuperCarController.cs
+++ VehicleShowroom.Web/Controllers/SuperCarController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 using VehicleShowroom.Data;
 
 namespace VehicleShowroom.Web.Controllers
 {
+    using static VehicleShowroom.Common.EntityValidationConstants;
+
     public class SuperCarController : Controller
     {
         private readonly VehicleDbContext context;
@@ -33,7 +36,7 @@ namespace VehicleShowroom.Web.Controllers
                     VehicleType = c.Vehicle.VehicleType,
                     Make = c.Vehicle.Make,
                     Model = c.Vehicle.Model,
-                    Year = c.Vehicle.Year.ToString(),
+                    Year = c.Vehicle.Year.ToString(YearFormating),
                     Price = c.Vehicle.Price,
                     Color = c.Vehicle.Color,
                     FuelType = c.Vehicle.FuelType,
@@ -53,6 +56,90 @@ namespace VehicleShowroom.Web.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(superCar);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var vehicle = await context
+               .SuperCars
+               .Include(c => c.Vehicle)
+               .Where(c => c.VehicleId == id)
+               .Select(c => new SuperCarEditVieModel
+               {
+
+                   VehicleType = c.Vehicle.VehicleType,
+                   Make = c.Vehicle.Make,
+                   Model = c.Vehicle.Model,
+                   Year = c.Vehicle.Year.ToString(YearFormating),
+                   Price = c.Vehicle.Price,
+                   Color = c.Vehicle.Color,
+                   FuelType = c.Vehicle.FuelType,
+                   ImageUrl = c.Vehicle.ImageUrl,
+
+                   SuperCarId = c.SuperCarId,
+                   MaxSpeed = c.MaxSpeed,
+                   Weight = c.Weight,
+                   Kilometers = c.Kilometers,
+                   Doors = c.NumberOfDoors,
+                   Description = c.Description,
+                   Transmission = c.Transmission,
+                   HorsePower = c.HorsePower
+               })
+                .FirstOrDefaultAsync();
+
+            if (vehicle == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(vehicle);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(SuperCarEditVieModel models)
+        {
+            bool IsYearValid = DateTime
+               .TryParseExact(models.Year, YearFormating, CultureInfo.InvariantCulture, DateTimeStyles.None,
+               out DateTime yearValid);
+
+            if (!IsYearValid)
+            {
+                ModelState.AddModelError(nameof(models.Year), "The Year must be in the following format: dd/MM/yyyy");
+
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(models);
+            }
+
+            var superCar = await context
+                .SuperCars
+                .Include(c => c.Vehicle)
+                .FirstOrDefaultAsync(c => c.SuperCarId == models.SuperCarId);
+            if (superCar == null)
+            {
+                return NotFound();
+            }
+
+            superCar.Vehicle.VehicleType = models.VehicleType;
+            superCar.Vehicle.Make = models.Make;
+            superCar.Vehicle.Model = models.Model;
+            superCar.Vehicle.Year = yearValid;
+            superCar.Vehicle.Price = models.Price;
+            superCar.Vehicle.Color = models.Color;
+            superCar.Vehicle.FuelType = models.FuelType;
+            superCar.Vehicle.ImageUrl = models.ImageUrl;
+
+            superCar.Kilometers = models.Kilometers;
+            superCar.NumberOfDoors = models.Doors;
+            superCar.Description = models.Description ?? string.Empty;
+            superCar.Transmission = models.Transmission ?? string.Empty;
+            superCar.HorsePower = models.HorsePower;
+            superCar.MaxSpeed = models.MaxSpeed ?? string.Empty;
+            superCar.Weight = models.Weight ?? string.Empty;
+
+            await context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ VehicleShowroom.Web/Model/SuperCar/SuperCarEditVieModel.cs
@@ -0,0 +1,43 @@
+﻿namespace VehicleShowroom.Web
+{
+    using System.ComponentModel.DataAnnotations;
+    using static VehicleShowroom.Common.EntityValidationMessages;
+
+    public class SuperCarEditVieModel
+    {
+        public int VehicleId { get; set; }
+
+        [Required(ErrorMessage = VehicleTypeMessages)]
+        public string VehicleType { get; set; } = null!;
+
+        [Required(ErrorMessage = VehicleMakeMessages)]
+        public string Make { get; set; } = null!;
+
+        [Required(ErrorMessage = VehicleModelMessages)]
+        public string Model { get; set; } = null!;
+
+        [Required]
+        public string Year { get; set; } = null!;
+
+        [Required(ErrorMessage = VehiclePriceMessages)]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = VehicleColorMessages)]
+        public string Color { get; set; } = null!;
+
+        [Required(ErrorMessage = VehicleFuelTypeMessages)]
+        public string FuelType { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        public int SuperCarId { get; set; }
+        public string? MaxSpeed { get; set; }
+        public string? Weight { get; set; }
+        public int Kilometers { get; set; }
+        public int Doors { get; set; }
+        public string? Description { get; set; }
+        public string? Transmission { get; set; }
+        public int HorsePower { get; set; }
+    }
+}
